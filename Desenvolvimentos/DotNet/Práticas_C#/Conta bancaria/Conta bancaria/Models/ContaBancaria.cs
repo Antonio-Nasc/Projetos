252b544a165146d661f83838ccd42772009c0fd4
José Antônio Do Nascimento Neto <joseeantonioo2000@gmail.com>
2023-05-19 14:14:32 -0300

    Novas atualizações

--- Desenvolvimentos/DotNet/Práticas_C#/Conta bancaria/Conta bancaria/Models/ContaBancaria.cs
+++ Desenvolvimentos/DotNet/Práticas_C#/Conta bancaria/Conta bancaria/Models/ContaBancaria.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace Conta_bancaria.Models
 {
-    internal class ContaBancaria
+    public class ContaBancaria
     {
+        public int NumeroDaConta { get; set; }
+        public double SaldoAtual { get; set; }
+
+        public DateTime Data = new DateTime();
+
+        public double Depositar(double deposita)
+        {
+            SaldoAtual += deposita;
+            return SaldoAtual;
+        }
+        public double Sacar(double saca)
+        {
+            if(SaldoAtual > saca) 
+            {
+                SaldoAtual -= saca;
+            }
+            else
+            {
+                Console.WriteLine("O valor de saque é maior que seu saldo na conta");
+            }
+            return SaldoAtual;
+        }
+        public double VerificarSaldo()
+        {
+           return SaldoAtual;
+        }
+       public double Transferir(double transfere)
+        {
+            if(SaldoAtual > transfere)
+            {
+                SaldoAtual -= transfere;
+            }
+            else { Console.WriteLine("Saldo na conta é menor do que deseja transferir."); }
+            return SaldoAtual;
+        }
+
     }
 }
--- Desenvolvimentos/DotNet/Práticas_C#/Conta bancaria/Conta bancaria/Program.cs
+++ Desenvolvimentos/DotNet/Práticas_C#/Conta bancaria/Conta bancaria/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Conta_bancaria.Models;
+
+ContaBancaria conta1 = new ContaBancaria();
+
+conta1.SaldoAtual = 250;
+conta1.Sacar(50);
+conta1.Depositar(150);
+//Console.WriteLine(conta1.VerificarSaldo());
+ContaBancaria conta2 = new ContaBancaria();
+conta2.SaldoAtual = 100;
+Console.WriteLine(conta2.SaldoAtual);
+conta1.SaldoAtual += conta2.Transferir(40);
+Console.WriteLine(conta2.SaldoAtual);
+
+
