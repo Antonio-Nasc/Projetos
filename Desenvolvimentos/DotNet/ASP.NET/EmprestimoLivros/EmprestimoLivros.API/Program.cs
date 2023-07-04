6242801e99d38947f3079ec387e98bca3979668c
José Antônio Do Nascimento Neto <joseeantonioo2000@gmail.com>
2023-07-04 19:45:55 -0300

    Injeção de dependência e configurando o repositório

--- Desenvolvimentos/DotNet/ASP.NET/EmprestimoLivros/EmprestimoLivros.API/Program.cs
+++ Desenvolvimentos/DotNet/ASP.NET/EmprestimoLivros/EmprestimoLivros.API/Program.cs
@@ -1,11 +1,21 @@
+using EmprestimoLivros.API.Interfaces;
+using EmprestimoLivros.API.Models;
+using EmprestimoLivros.API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ControleEmprestimoLivroContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();  
 
 var app = builder.Build();
 
---
+++ Desenvolvimentos/DotNet/ASP.NET/EmprestimoLivros/EmprestimoLivros.API/Repositories/ClienteRepository.cs
@@ -0,0 +1,46 @@
+﻿using EmprestimoLivros.API.Interfaces;
+using EmprestimoLivros.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.API.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly ControleEmprestimoLivroContext _context;
+
+        public ClienteRepository(ControleEmprestimoLivroContext context)
+        {
+            _context = context;
+        }
+        public void Alterar(Cliente cliente)
+        {
+            _context.Entry(cliente).State = EntityState.Modified;
+        }
+
+        public void Excluir(Cliente cliente)
+        {
+            _context.Cliente.Remove(cliente);
+        }
+
+        public void Incluir(Cliente cliente)
+        {
+            _context.Cliente.Add(cliente);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<Cliente> SelecionarByPk(int id)
+        {
+            var cliente = await _context.Cliente.Where(x => x.Id == id).FirstOrDefaultAsync();
+            return cliente;
+        }
+
+        public async Task<IEnumerable<Cliente>> SelecionarTodos()
+        {
+            return await _context.Cliente.ToListAsync();
+        }
+    }
+}
